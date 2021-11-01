3b58af3c08544168f470bc1d0d8edf99e4c50bb6
WhatTheBlock <40543229@gm.nfu.edu.tw>
2021-11-01 19:10:17 +0800

    Bug fixes
    - 關閉計算機改為由icon控制
    - 修正無效的OnClickListener
    - 修正長按icon後未關閉計算機
    - 修正START鍵會重複建立icon的問題
    - 新增刪除鍵

--- app/src/main/java/tw/wtb/floatcalculator/MainActivity.kt
+++ app/src/main/java/tw/wtb/floatcalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package tw.wtb.floatcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.widget.EditText
 import androidx.core.content.res.ResourcesCompat
 import com.hjq.permissions.OnPermissionCallback
 import com.hjq.permissions.Permission
@@ -11,56 +12,60 @@ import com.hjq.toast.ToastUtils
 import com.hjq.xtoast.XToast
 import com.hjq.xtoast.draggable.SpringDraggable
 import tw.wtb.floatcalculator.databinding.ActivityMainBinding
-import tw.wtb.floatcalculator.databinding.CalculatorViewBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var calculatorViewBinding: CalculatorViewBinding
+    private lateinit var mCalculatorIcon: XToast<XToast<*>>
+    private lateinit var mCalculatorView: XToast<XToast<*>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        calculatorViewBinding = CalculatorViewBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        val mCalculatorView = XToast<XToast<*>>(application)
+        mCalculatorIcon = XToast<XToast<*>>(application).apply {
+            setContentView(R.layout.calculator_icon)
+            setGravity(Gravity.END)
+            setYOffset(-500)
+            setDraggable(SpringDraggable())
+            setImageDrawable(android.R.id.icon, ResourcesCompat.getDrawable(resources, R.mipmap.ic_launcher, null))
+            setOnClickListener(android.R.id.icon) { _, viewIcon ->
+                if(!mCalculatorView.isShow) {
+                    mCalculatorView.showAsDropDown(viewIcon, Gravity.BOTTOM)
+                }
+                else {
+                    mCalculatorView.cancel()
+                }
+            }
+            setOnLongClickListener(android.R.id.icon) { toastIcon, _ ->
+                ToastUtils.show("已關閉計算機")
+                mCalculatorView.cancel()
+                toastIcon.cancel()
+                true
+            }
+        }
+
+        mCalculatorView = XToast<XToast<*>>(application).apply {
+            setContentView(R.layout.calculator_view)
+            setDraggable(SpringDraggable())
+            setOnClickListener(R.id.btnClear) { toastCalc, _ ->
+                toastCalc.findViewById<EditText>(R.id.result).setText("0")
+            }
+        }
 
         binding.btnStart.setOnClickListener {
             XXPermissions.with(this)
                 .permission(Permission.SYSTEM_ALERT_WINDOW)
                 .request (object : OnPermissionCallback {
                     override fun onGranted(permissions: List<String>, all: Boolean) {
-                        XToast<XToast<*>>(application).apply {
-                            setContentView(R.layout.calculator_icon)
-                            setGravity(Gravity.END)
-                            setYOffset(-500)
-                            setDraggable(SpringDraggable())
-                            setImageDrawable(android.R.id.icon, ResourcesCompat.getDrawable(resources, R.mipmap.ic_launcher, null))
-                            setOnClickListener(android.R.id.icon) { _, view ->
-                                mCalculatorView.setContentView(R.layout.calculator_view)
-                                mCalculatorView.setDraggable(SpringDraggable())
-                                mCalculatorView.setOnClickListener(R.id.btnClose) { toastCalc, _ ->
-                                    toastCalc.cancel()
-                                }
-                                mCalculatorView.showAsDropDown(view, Gravity.BOTTOM)
-                            }
-                            setOnLongClickListener(android.R.id.icon) { toast, _ ->
-                                ToastUtils.show("已關閉計算機")
-                                toast.cancel()
-                                true
-                            }
-                        }.show()
+                        mCalculatorIcon.show()
                     }
                     override fun onDenied(permissions: List<String>, never: Boolean) {
                         if(ToastUtils.isInit())
                             ToastUtils.show("未授予權限")
                     }
                 })
-        }
-
-        calculatorViewBinding.btnClear.setOnClickListener {
-            calculatorViewBinding.result.setText("0")
         }
     }
 }
